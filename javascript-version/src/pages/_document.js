0939f9e01a5d6e16051da2e0fe993c5d5621c01b
neelbrahmakshatriya <brahma.neel.j@gmail.com>
2022-03-16 12:23:59 +0530

    refactor(multi): refactor multiple items
    Remove .env files
    Updates yarn.lock files
    Change favicon file paths and update it in _document file
    Remove github stars and add TextFeild in small screen

--- javascript-version/src/pages/_document.js
+++ javascript-version/src/pages/_document.js
@@ -21,8 +21,8 @@ class CustomDocument extends Document {
             rel='stylesheet'
             href='https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700&display=swap'
           />
-          <link rel='apple-touch-icon' sizes='180x180' href='/apple-touch-icon.png' />
-          <link rel='shortcut icon' href='/favicon.png' />
+          <link rel='apple-touch-icon' sizes='180x180' href='/images/apple-touch-icon.png' />
+          <link rel='shortcut icon' href='/images/favicon.png' />
         </Head>
         <body>
           <Main />
--- typescript-version/src/layouts/components/vertical/AppBarContent.tsx
+++ typescript-version/src/layouts/components/vertical/AppBarContent.tsx
@@ -44,38 +44,34 @@ const AppBarContent = (props: Props) => {
             <Menu />
           </IconButton>
         ) : null}
-        {hiddenSm ? (
-          <IconButton color='inherit'>
-            <Magnify />
-          </IconButton>
-        ) : (
-          <TextField
-            size='small'
-            sx={{ '& .MuiOutlinedInput-root': { borderRadius: 4 } }}
-            InputProps={{
-              startAdornment: (
-                <InputAdornment position='start'>
-                  <Magnify fontSize='small' />
-                </InputAdornment>
-              )
-            }}
-          />
-        )}
+        <TextField
+          size='small'
+          sx={{ '& .MuiOutlinedInput-root': { borderRadius: 4 } }}
+          InputProps={{
+            startAdornment: (
+              <InputAdornment position='start'>
+                <Magnify fontSize='small' />
+              </InputAdornment>
+            )
+          }}
+        />
       </Box>
       <Box className='actions-right' sx={{ display: 'flex', alignItems: 'center' }}>
-        <Box
-          component='a'
-          target='_blank'
-          rel='noreferrer'
-          sx={{ mr: 4, display: 'flex' }}
-          href='https://github.com/themeselection/materio-mui-react-nextjs-admin-template-free'
-        >
-          <img
-            height={24}
-            alt='github stars'
-            src='https://img.shields.io/github/stars/themeselection/materio-mui-react-nextjs-admin-template-free?style=social'
-          />
-        </Box>
+        {hiddenSm ? null : (
+          <Box
+            component='a'
+            target='_blank'
+            rel='noreferrer'
+            sx={{ mr: 4, display: 'flex' }}
+            href='https://github.com/themeselection/materio-mui-react-nextjs-admin-template-free'
+          >
+            <img
+              height={24}
+              alt='github stars'
+              src='https://img.shields.io/github/stars/themeselection/materio-mui-react-nextjs-admin-template-free?style=social'
+            />
+          </Box>
+        )}
         <ModeToggler settings={settings} saveSettings={saveSettings} />
         <NotificationDropdown />
         <UserDropdown />
