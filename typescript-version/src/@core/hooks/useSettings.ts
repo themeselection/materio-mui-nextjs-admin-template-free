22784ab18831a59d3968bb3a5d255dd8480689f9
Re <reivi96@gmail.com>
2023-03-30 13:22:35 +0000

    Update 250 files

--- typescript-version/src/@core/hooks/useSettings.ts
+++
@@ -1,4 +0,0 @@
-import { useContext } from 'react'
-import { SettingsContext, SettingsContextValue } from 'src/@core/context/settingsContext'
-
-export const useSettings = (): SettingsContextValue => useContext(SettingsContext)
