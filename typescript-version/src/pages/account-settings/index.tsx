b0eecad8beac4186b60218a870f9448c397ff23f
karsonchancwk <wingkinchancwk@gmail.com>
2023-11-12 19:52:30 +0800

    form looks decent

--- typescript-version/src/pages/account-settings/index.tsx
+++ typescript-version/src/pages/account-settings/index.tsx
@@ -18,7 +18,7 @@ import FormControl from '@mui/material/FormControl'
 
 // ** Third Party Styles Imports
 import 'react-datepicker/dist/react-datepicker.css'
-import { ArrowDownDropCircle } from 'mdi-material-ui'
+import { ArrowDownDropCircle, ArrowUpDropCircle } from 'mdi-material-ui'
 
 import UserPortfolio from 'src/views/dashboard/UserPortfolio'
 import ReccETF from 'src/views/dashboard/ReccETF'
@@ -26,94 +26,94 @@ import ReccETF from 'src/views/dashboard/ReccETF'
 
 const columns: GridColDef[] = [
   { field: 'from', headerName: 'From', width: 130 },
-  { field: 'subject', headerName: 'Subject', width: 400, editable: true },
-  { field: 'Received', headerName: 'Received', width: 180, editable: true }
-]
-const rows = [
-  {
-    id: 0,
-    from: Math.random() < 0.5 ? 'Yigit Sen' : 'Betty Lin',
-    subject: Math.random() < 0.5 ? 'Touch Ups on Previous Meetings' : 'Meeting Summary',
-    Received: `${new Date(2022, Math.floor(Math.random() * 12), Math.floor(Math.random() * 28) + 1)
-      .toISOString()
-      .slice(0, 10)}`
-  },
-  {
-    id: 1,
-    from: Math.random() < 0.5 ? 'Yigit Sen' : 'Betty Lin',
-    subject: Math.random() < 0.5 ? 'Touch Ups on Previous Meetings' : 'Meeting Summary',
-    Received: `${new Date(2022, Math.floor(Math.random() * 12), Math.floor(Math.random() * 28) + 1)
-      .toISOString()
-      .slice(0, 10)}`
-  },
-  {
-    id: 2,
-    from: Math.random() < 0.5 ? 'Yigit Sen' : 'Betty Lin',
-    subject: Math.random() < 0.5 ? 'Touch Ups on Previous Meetings' : 'Meeting Summary',
-    Received: `${new Date(2022, Math.floor(Math.random() * 12), Math.floor(Math.random() * 28) + 1)
-      .toISOString()
-      .slice(0, 10)}`
-  },
-  {
-    id: 3,
-    from: Math.random() < 0.5 ? 'Yigit Sen' : 'Betty Lin',
-    subject: Math.random() < 0.5 ? 'Touch Ups on Previous Meetings' : 'Meeting Summary',
-    Received: `${new Date(2022, Math.floor(Math.random() * 12), Math.floor(Math.random() * 28) + 1)
-      .toISOString()
-      .slice(0, 10)}`
-  },
-  {
-    id: 4,
-    from: Math.random() < 0.5 ? 'Yigit Sen' : 'Betty Lin',
-    subject: Math.random() < 0.5 ? 'Touch Ups on Previous Meetings' : 'Meeting Summary',
-    Received: `${new Date(2022, Math.floor(Math.random() * 12), Math.floor(Math.random() * 28) + 1)
-      .toISOString()
-      .slice(0, 10)}`
-  },
-  {
-    id: 5,
-    from: Math.random() < 0.5 ? 'Yigit Sen' : 'Betty Lin',
-    subject: Math.random() < 0.5 ? 'Touch Ups on Previous Meetings' : 'Meeting Summary',
-    Received: `${new Date(2022, Math.floor(Math.random() * 12), Math.floor(Math.random() * 28) + 1)
-      .toISOString()
-      .slice(0, 10)}`
-  },
-  {
-    id: 6,
-    from: Math.random() < 0.5 ? 'Yigit Sen' : 'Betty Lin',
-    subject: Math.random() < 0.5 ? 'Touch Ups on Previous Meetings' : 'Meeting Summary',
-    Received: `${new Date(2022, Math.floor(Math.random() * 12), Math.floor(Math.random() * 28) + 1)
-      .toISOString()
-      .slice(0, 10)}`
-  },
-  {
-    id: 7,
-    from: Math.random() < 0.5 ? 'Yigit Sen' : 'Betty Lin',
-    subject: Math.random() < 0.5 ? 'Touch Ups on Previous Meetings' : 'Meeting Summary',
-    Received: `${new Date(2022, Math.floor(Math.random() * 12), Math.floor(Math.random() * 28) + 1)
-      .toISOString()
-      .slice(0, 10)}`
-  },
-  {
-    id: 8,
-    from: Math.random() < 0.5 ? 'Yigit Sen' : 'Betty Lin',
-    subject: Math.random() < 0.5 ? 'Touch Ups on Previous Meetings' : 'Meeting Summary',
-    Received: `${new Date(2022, Math.floor(Math.random() * 12), Math.floor(Math.random() * 28) + 1)
-      .toISOString()
-      .slice(0, 10)}`
-  }
+  { field: 'topic', headerName: 'Topic', width: 400, editable: true },
+  { field: 'received', headerName: 'received', width: 180, editable: true }
 ]
 
 const AccountSettings = () => {
   const [openNewCommu, togOpenNewCommu] = useState<any>(false)
   const [commu, setCommu] = useState<any>({ topic: '', sentBy: '', content: '' })
+  const [rows, setRows] = useState<any>([
+    {
+      id: 0,
+      from: Math.random() < 0.5 ? 'Yigit Sen' : 'Betty Lin',
+      topic: Math.random() < 0.5 ? 'Touch Ups on Previous Meetings' : 'Meeting Summary',
+      received: `${new Date(2022, Math.floor(Math.random() * 12), Math.floor(Math.random() * 28) + 1)
+        .toISOString()
+        .slice(0, 10)}`
+    },
+    {
+      id: 1,
+      from: Math.random() < 0.5 ? 'Yigit Sen' : 'Betty Lin',
+      topic: Math.random() < 0.5 ? 'Touch Ups on Previous Meetings' : 'Meeting Summary',
+      received: `${new Date(2022, Math.floor(Math.random() * 12), Math.floor(Math.random() * 28) + 1)
+        .toISOString()
+        .slice(0, 10)}`
+    },
+    {
+      id: 2,
+      from: Math.random() < 0.5 ? 'Yigit Sen' : 'Betty Lin',
+      topic: Math.random() < 0.5 ? 'Touch Ups on Previous Meetings' : 'Meeting Summary',
+      received: `${new Date(2022, Math.floor(Math.random() * 12), Math.floor(Math.random() * 28) + 1)
+        .toISOString()
+        .slice(0, 10)}`
+    },
+    {
+      id: 3,
+      from: Math.random() < 0.5 ? 'Yigit Sen' : 'Betty Lin',
+      topic: Math.random() < 0.5 ? 'Touch Ups on Previous Meetings' : 'Meeting Summary',
+      received: `${new Date(2022, Math.floor(Math.random() * 12), Math.floor(Math.random() * 28) + 1)
+        .toISOString()
+        .slice(0, 10)}`
+    },
+    {
+      id: 4,
+      from: Math.random() < 0.5 ? 'Yigit Sen' : 'Betty Lin',
+      topic: Math.random() < 0.5 ? 'Touch Ups on Previous Meetings' : 'Meeting Summary',
+      received: `${new Date(2022, Math.floor(Math.random() * 12), Math.floor(Math.random() * 28) + 1)
+        .toISOString()
+        .slice(0, 10)}`
+    },
+    {
+      id: 5,
+      from: Math.random() < 0.5 ? 'Yigit Sen' : 'Betty Lin',
+      topic: Math.random() < 0.5 ? 'Touch Ups on Previous Meetings' : 'Meeting Summary',
+      received: `${new Date(2022, Math.floor(Math.random() * 12), Math.floor(Math.random() * 28) + 1)
+        .toISOString()
+        .slice(0, 10)}`
+    },
+    {
+      id: 6,
+      from: Math.random() < 0.5 ? 'Yigit Sen' : 'Betty Lin',
+      topic: Math.random() < 0.5 ? 'Touch Ups on Previous Meetings' : 'Meeting Summary',
+      received: `${new Date(2022, Math.floor(Math.random() * 12), Math.floor(Math.random() * 28) + 1)
+        .toISOString()
+        .slice(0, 10)}`
+    },
+    {
+      id: 7,
+      from: Math.random() < 0.5 ? 'Yigit Sen' : 'Betty Lin',
+      topic: Math.random() < 0.5 ? 'Touch Ups on Previous Meetings' : 'Meeting Summary',
+      received: `${new Date(2022, Math.floor(Math.random() * 12), Math.floor(Math.random() * 28) + 1)
+        .toISOString()
+        .slice(0, 10)}`
+    },
+    {
+      id: 8,
+      from: Math.random() < 0.5 ? 'Yigit Sen' : 'Betty Lin',
+      topic: Math.random() < 0.5 ? 'Touch Ups on Previous Meetings' : 'Meeting Summary',
+      received: `${new Date(2022, Math.floor(Math.random() * 12), Math.floor(Math.random() * 28) + 1)
+        .toISOString()
+        .slice(0, 10)}`
+    }
+  ])
 
   return (
     <Grid container spacing={2}>
       <Grid item xs={12} md={8}>
         <Card>
           {/* 2 Rows of Client Info */}
-          <Card>
+          <Card sx={{ mb: 4 }}>
             {/* Client info Row 1 */}
             <Box sx={{ display: 'flex', flexDirection: 'row', ml: 4, mt: 4 }}>
               <Box sx={{ width: '30%', display: 'flex', flexDirection: 'row' }}>
@@ -132,9 +132,8 @@ const AccountSettings = () => {
                 <Typography sx={{ fontWeight: 600, fontSize: '0.875rem', color: '#0297DB' }}>Since</Typography>
               </Box>
             </Box>
-
             {/* Client info Row 2 */}
-            <Box sx={{ display: 'flex', flexDirection: 'row', ml: 4 }}>
+            <Box sx={{ display: 'flex', flexDirection: 'row', ml: 4, mb: 4 }}>
               <Box sx={{ width: '30%', display: 'flex', flexDirection: 'row' }}>
                 <Typography sx={{ fontWeight: 600, fontSize: '1.5rem' }}>ABC Co. Ltd</Typography>
               </Box>
@@ -151,78 +150,105 @@ const AccountSettings = () => {
                 <Typography sx={{ fontWeight: 600, fontSize: '0.875rem' }}>1999</Typography>
               </Box>
             </Box>
-          </Card>
-
-          {/* New Conversation */}
-          <Card>
-            <Box sx={{ display: 'flex', justifyContent: 'space-between' }}>
+            {/* New Conversation */}
+            <Box sx={{ display: 'flex', justifyContent: 'space-between', mb: 5, alignItem: 'center' }}>
               <Typography sx={{ fontWeight: 600, fontSize: '0.875rem', ml: 4, color: '#0297DB', mt: 2 }}>
                 Recent Communications
               </Typography>
               <Button
                 onClick={() => togOpenNewCommu(!openNewCommu)}
+                sx={{ mr: 4 }}
                 variant='contained'
                 size='small'
-                startIcon={<ArrowDownDropCircle />}
+                // startIcon={<ArrowDownDropCircle />}
               >
-                <Typography sx={{ fontSize: '0.875rem', color: 'white' }}>New Conversation</Typography>
-                <ArrowDownDropCircle />
+                <Typography sx={{ fontSize: '0.875rem', color: 'white', mr: 3 }}>New Conversation </Typography>
+                {openNewCommu ? <ArrowUpDropCircle /> : <ArrowDownDropCircle />}
               </Button>
             </Box>
+            {openNewCommu && (
+              <form
+                noValidate
+                autoComplete='off'
+                onSubmit={e => {
+                  e.preventDefault()
+                  console.log(commu)
+                  setRows([
+                    ...rows,
+                    {
+                      id: rows.length,
+                      from: commu.sentBy,
+                      topic: commu.topic,
+                      received: new Date().toISOString().slice(0, 10)
+                    }
+                  ])
+                }}
+              >
+                <Box sx={{ display: 'flex', flexDirection: 'row', mx: 6, my: 4, gap: 10 }}>
+                  <TextField
+                    label='Topic'
+                    id='topic'
+                    sx={{ flexGrow: 1 }}
+                    value={commu?.topic}
+                    onChange={e => setCommu({ ...commu, topic: e.target.value })}
+                  />
+                  <FormControl sx={{ mr: 10 }} id='sentBy'>
+                    <InputLabel id='sentBy-label'>Sent by</InputLabel>
+                    <Select
+                      label='Sent By'
+                      labelId='sentBy-label'
+                      defaultValue=''
+                      value={commu?.sentBy}
+                      onChange={e => setCommu({ ...commu, sentBy: e.target.value })}
+                    >
+                      <MenuItem value=''>Select Sender</MenuItem>
+                      <MenuItem value='Betty Lin'>Betty Lin</MenuItem>
+                      <MenuItem value='Yigit Sen'>Yigit Sen</MenuItem>
+                    </Select>
+                  </FormControl>
+                </Box>
+                <Box sx={{ display: 'flex', flexDirection: 'row', m: 6 }}>
+                  <TextField
+                    fullWidth
+                    rows={10}
+                    multiline
+                    label='Main Content'
+                    placeholder='Input your communication content here'
+                    id='content'
+                    value={commu?.content}
+                    onChange={e => setCommu({ ...commu, content: e.target.value })}
+                  />
+                </Box>
 
-            <form
-              noValidate
-              autoComplete='off'
-              onSubmit={e => {
-                e.preventDefault()
-                console.log(commu)
-              }}
-            >
-              <Box sx={{ display: 'flex', flexDirection: 'row', mx: 6, my: 4, gap: 10 }}>
-                <TextField
-                  label='Topic'
-                  id='topic'
-                  sx={{ flexGrow: 1 }}
-                  onChange={e => setCommu({ ...commu, topic: e.target.value })}
-                />
-                <FormControl sx={{ mr: 10 }} id='sentBy'>
-                  <InputLabel id='sentBy-label'>Age</InputLabel>
-                  <Select
-                    label='Sent By'
-                    labelId='sentBy-label'
-                    defaultValue=''
-                    onChange={e => setCommu({ ...commu, sentBy: e.target.value })}
+                {/* Submit / Cancel */}
+                <Box sx={{ display: 'flex', gap: 3, mb: 7, ml: 6 }}>
+                  <Button size='large' variant='contained' type='submit'>
+                    Submit
+                  </Button>
+                  <Button
+                    size='large'
+                    variant='outlined'
+                    onClick={() => setCommu({ topic: '', sentBy: '', content: '' })}
                   >
-                    <MenuItem value='Betty Lin'>Betty Lin</MenuItem>
-                    <MenuItem value='Yigit Sen'>Yigit Sen</MenuItem>
-                  </Select>
-                </FormControl>
-              </Box>
-              <Box sx={{ display: 'flex', flexDirection: 'row', m: 6 }}>
-                <TextField
-                  fullWidth
-                  rows={10}
-                  multiline
-                  label='Main Content'
-                  placeholder='Input your communication content here'
-                  id='content'
-                  onChange={e => setCommu({ ...commu, content: e.target.value })}
-                />
-              </Box>
-              <Button size='large' variant='contained' sx={{ marginBottom: 7 }} type='submit'>
-                Login
-              </Button>
-            </form>
+                    Cancel
+                  </Button>
+                </Box>
+              </form>
+            )}
           </Card>
+
           {/* Communication Log */}
           <Card>
             <DataGrid rows={rows} columns={columns} checkboxSelection sx={{ mt: 4 }} />
           </Card>
         </Card>
+
+        {/* Portfolio */}
         <Grid item xs={12} md={12} lg={12}>
           <UserPortfolio />
         </Grid>
       </Grid>
+
       {/* Right side pie charts*/}
       <Grid item xs={12} md={4}>
         <ReccETF />
--- typescript-version/src/views/dashboard/UserPortfolio.tsx
+++ typescript-version/src/views/dashboard/UserPortfolio.tsx
@@ -19,78 +19,39 @@ interface DataType {
 }
 
 const depositData = [
-  
   {
     amount: '+0.44',
     title: 'Equity Income Premium ETF',
     subtitle: 'JEPI',
-    currentVal:'$99M'
+    currentVal: '$99M'
   },
   {
     amount: '+$0.68',
     title: 'Global Security Equity',
     subtitle: 'JGLO',
-    currentVal:'$1M'
+    currentVal: '$1M'
   }
 ]
-
-
 
 const UserPortfolio = () => {
   return (
     <Card>
-    <Grid>
-    <PortfolioTable/>
-    <h1></h1>
-    <Card sx={{ display: 'flex', justifyContent: 'space-between', flexDirection: ['column', 'column', 'row'] }}>
-      <Box sx={{ width: '100%' }}>
-        <CardHeader
-          title='ETF Holdings'
-          sx={{ pt: 5.5, alignItems: 'center', '& .MuiCardHeader-action': { mt: 0.6 } }}
-          action={<Typography variant='caption'>View All</Typography>}
-          titleTypographyProps={{
-            variant: 'h6',
-            sx: { lineHeight: '1.6 !important', letterSpacing: '0.15px !important' }
-          }}
-        />
-        <CardContent sx={{ pb: theme => `${theme.spacing(5.5)} !important` }}>
-          <Box
-            sx={{
-              display: 'flex',
-              alignItems: 'center',
-              mb: 6
-            }}
-          >
-            <Box
-              sx={{
-                ml: 4,
-                width: '100%',
-                display: 'flex',
-                flexWrap: 'wrap',
-                alignItems: 'center',
-                justifyContent: 'space-between'
+      <Grid>
+        <PortfolioTable />
+        <h1></h1>
+        <Card sx={{ display: 'flex', justifyContent: 'space-between', flexDirection: ['column', 'column', 'row'] }}>
+          <Box sx={{ width: '100%' }}>
+            <CardHeader
+              title='ETF Holdings'
+              sx={{ pt: 5.5, alignItems: 'center', '& .MuiCardHeader-action': { mt: 0.6 } }}
+              action={<Typography variant='caption'>View All</Typography>}
+              titleTypographyProps={{
+                variant: 'h6',
+                sx: { lineHeight: '1.6 !important', letterSpacing: '0.15px !important' }
               }}
-            >
-              <Box sx={{ width: '20%', display: 'flex', flexDirection: 'column' }}>
-                <Typography sx={{ fontWeight: 600, fontSize: '0.875rem' }}>Ticker</Typography>
-              </Box>
-              <Box sx={{ width: '30%', display: 'flex', flexDirection: 'column' }}>
-                <Typography sx={{ fontWeight: 600, fontSize: '0.875rem' }}>ETF</Typography>
-              </Box>
-              <Box sx={{ width: '20%', display: 'flex', flexDirection: 'column' }}>
-                <Typography sx={{ fontWeight: 600, fontSize: '0.875rem' }}>Current Value</Typography>
-              </Box>
-              <Typography variant='subtitle2' sx={{ width: '20%', textAlign: 'right' }}>
-                % Change
-              </Typography>
-            </Box>
-          </Box>
-          {depositData.map((item: DataType, index: number) => {
-            return (
-              <Box
-                key={item.title}
-                sx={{ display: 'flex', alignItems: 'center', mb: index !== depositData.length - 1 ? 6 : 0 }}
-              >
+            />
+            <CardContent sx={{ pb: theme => `${theme.spacing(5.5)} !important` }}>
+              <Box sx={{ display: 'flex', alignItems: 'center', mb: 6 }}>
                 <Box
                   sx={{
                     ml: 4,
@@ -101,27 +62,61 @@ const UserPortfolio = () => {
                     justifyContent: 'space-between'
                   }}
                 >
-                  
                   <Box sx={{ width: '20%', display: 'flex', flexDirection: 'column' }}>
-                    <Typography sx={{ fontWeight: 600, fontSize: '0.875rem' }}>{item.subtitle}</Typography>
+                    <Typography sx={{ fontWeight: 600, fontSize: '0.875rem' }}>Ticker</Typography>
                   </Box>
                   <Box sx={{ width: '30%', display: 'flex', flexDirection: 'column' }}>
-                    <Typography sx={{ fontWeight: 600, fontSize: '0.875rem', color:'#0297DB'}}>{item.title}</Typography>
+                    <Typography sx={{ fontWeight: 600, fontSize: '0.875rem' }}>ETF</Typography>
                   </Box>
                   <Box sx={{ width: '20%', display: 'flex', flexDirection: 'column' }}>
-                    <Typography sx={{ fontSize: '0.875rem' }}>{item.currentVal}</Typography>
+                    <Typography sx={{ fontWeight: 600, fontSize: '0.875rem' }}>Current Value</Typography>
                   </Box>
-                  <Typography variant='subtitle2' sx={{ fontWeight: 600, color: 'success.main', width: '20%', textAlign: 'right' }}>
-                    {item.amount}
+                  <Typography variant='subtitle2' sx={{ width: '20%', textAlign: 'right' }}>
+                    % Change
                   </Typography>
                 </Box>
               </Box>
-            )
-          })}
-        </CardContent>
-      </Box>
-    </Card>
-    </Grid>
+              {depositData.map((item: DataType, index: number) => {
+                return (
+                  <Box
+                    key={item.title}
+                    sx={{ display: 'flex', alignItems: 'center', mb: index !== depositData.length - 1 ? 6 : 0 }}
+                  >
+                    <Box
+                      sx={{
+                        ml: 4,
+                        width: '100%',
+                        display: 'flex',
+                        flexWrap: 'wrap',
+                        alignItems: 'center',
+                        justifyContent: 'space-between'
+                      }}
+                    >
+                      <Box sx={{ width: '20%', display: 'flex', flexDirection: 'column' }}>
+                        <Typography sx={{ fontWeight: 600, fontSize: '0.875rem' }}>{item.subtitle}</Typography>
+                      </Box>
+                      <Box sx={{ width: '30%', display: 'flex', flexDirection: 'column' }}>
+                        <Typography sx={{ fontWeight: 600, fontSize: '0.875rem', color: '#0297DB' }}>
+                          {item.title}
+                        </Typography>
+                      </Box>
+                      <Box sx={{ width: '20%', display: 'flex', flexDirection: 'column' }}>
+                        <Typography sx={{ fontSize: '0.875rem' }}>{item.currentVal}</Typography>
+                      </Box>
+                      <Typography
+                        variant='subtitle2'
+                        sx={{ fontWeight: 600, color: 'success.main', width: '20%', textAlign: 'right' }}
+                      >
+                        {item.amount}
+                      </Typography>
+                    </Box>
+                  </Box>
+                )
+              })}
+            </CardContent>
+          </Box>
+        </Card>
+      </Grid>
     </Card>
   )
 }
